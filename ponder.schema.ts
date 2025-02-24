510870d7d988af093ca388dcb0a56937a9255d8a
l3wi <1716917+l3wi@users.noreply.github.com>
2025-02-24 15:40:41 -0500

    Cleanup

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,73 +1,10 @@
 import { index, onchainTable } from "ponder";
 
-export const account = onchainTable("account", (t) => ({
-  address: t.hex().primaryKey(),
-  balance: t.bigint().notNull(),
-}));
-
-//   inputs: [
-//     { internalType: "address", name: "_messageTransmitter", type: "address" },
-//     { internalType: "uint32", name: "_messageBodyVersion", type: "uint32" },
-//   ],
-//   stateMutability: "nonpayable",
-//   type: "constructor",
-// },
-// {
-//   anonymous: false,
-//   inputs: [
-//     { indexed: true, internalType: "uint64", name: "nonce", type: "uint64" },
-//     {
-//       indexed: true,
-//       internalType: "address",
-//       name: "burnToken",
-//       type: "address",
-//     },
-//     {
-//       indexed: false,
-//       internalType: "uint256",
-//       name: "amount",
-//       type: "uint256",
-//     },
-//     {
-//       indexed: true,
-//       internalType: "address",
-//       name: "depositor",
-//       type: "address",
-//     },
-//     {
-//       indexed: false,
-//       internalType: "bytes32",
-//       name: "mintRecipient",
-//       type: "bytes32",
-//     },
-//     {
-//       indexed: false,
-//       internalType: "uint32",
-//       name: "destinationDomain",
-//       type: "uint32",
-//     },
-//     {
-//       indexed: false,
-//       internalType: "bytes32",
-//       name: "destinationTokenMessenger",
-//       type: "bytes32",
-//     },
-//     {
-//       indexed: false,
-//       internalType: "bytes32",
-//       name: "destinationCaller",
-//       type: "bytes32",
-//     },
-//   ],
-//   name: "DepositForBurn",
-//   type: "event",
-// },
-
 export const burnToken = onchainTable(
   "burnToken",
   (t) => ({
     id: t.hex().primaryKey(),
-    timestamp: t.timestamp().notNull(),
+    timestamp: t.bigint().notNull(),
     depositor: t.hex().notNull(),
     amount: t.bigint().notNull(),
     mintRecipient: t.hex().notNull(),
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { ponder } from "ponder:registry";
-import { account, burnToken } from "ponder:schema";
+import { burnToken } from "ponder:schema";
 
 ponder.on("tokenMessenger:DepositForBurn", async ({ event, context }) => {
   await context.db.insert(burnToken).values({
     id: event.transaction.hash,
-    timestamp: new Date(Number(event.block.timestamp) * 1000),
+    timestamp: event.block.timestamp,
     depositor: event.args.depositor,
     amount: event.args.amount,
     mintRecipient: event.args.mintRecipient,
