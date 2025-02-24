af56c7122d1c9ffeb214a3152505a8e7bf6351f9
l3wi <1716917+l3wi@users.noreply.github.com>
2025-02-24 15:35:25 -0500

    Add avax

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,7 +1,6 @@
 import { createConfig } from "ponder";
 import { http } from "viem";
 
-import { weth9Abi } from "./abis/weth9Abi";
 import { tokenMessengerAbi } from "./abis/tokenMessenger";
 
 export default createConfig({
@@ -14,6 +13,10 @@ export default createConfig({
     base: {
       chainId: 8453,
       transport: http(process.env.PONDER_RPC_URL_8453),
+    },
+    avalanche: {
+      chainId: 43114,
+      transport: http(process.env.PONDER_RPC_URL_43114),
     },
     optimism: {
       chainId: 10,
@@ -34,6 +37,7 @@ export default createConfig({
       startBlock: "latest",
       network: {
         mainnet: { address: "0xbd3fa81b58ba92a82136038b25adec7066af3155" },
+        avalanche: { address: "0x6b25532e1060ce10cc3b0a99e5683b91bfde6982" },
         optimism: { address: "0x2B4069517957735bE00ceE0fadAE88a26365528f" },
         arbitrum: { address: "0x19330d10D9Cc8751218eaf51E8885D058642E08A" },
         base: { address: "0x1682Ae6375C4E4A97e4B583BC394c861A46D8962" },
