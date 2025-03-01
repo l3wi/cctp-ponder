6fde4f74ee24154abb05f3de438913ef5de3363f
l3wi <1716917+l3wi@users.noreply.github.com>
2025-02-28 19:01:15 -0700

    ignore dupe

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import { ponder } from "ponder:registry";
 import { burnToken } from "ponder:schema";
 
 ponder.on("tokenMessenger:DepositForBurn", async ({ event, context }) => {
-  await context.db.insert(burnToken).values({
-    id: event.transaction.hash,
-    timestamp: event.block.timestamp,
-    depositor: event.args.depositor,
-    amount: event.args.amount,
-    mintRecipient: event.args.mintRecipient,
-    destinationDomain: BigInt(event.args.destinationDomain),
-  });
+  await context.db
+    .insert(burnToken)
+    .values({
+      id: event.transaction.hash,
+      timestamp: event.block.timestamp,
+      depositor: event.args.depositor,
+      amount: event.args.amount,
+      mintRecipient: event.args.mintRecipient,
+      destinationDomain: BigInt(event.args.destinationDomain),
+    })
+    .onConflictDoNothing();
 });
